eef1cf80d29dd77ba5c26d6e240f84fdea303c47
Fabio Epifani <epifab@gmail.com>
2017-06-23 11:06:46 +0100

    LastFmApi

--- lndngigs/main.py
+++ lndngigs/main.py
@@ -1,2 +1,23 @@
+import os
+import pylast
+
+
+class LastFmConfig:
+    def __init__(self):
+        self.LASTFM_API_KEY = os.environ["LASTFM_API_KEY"]
+        self.LASTFM_API_SECRET = os.environ["LASTFM_API_SECRET"]
+
+
+class LastFmApi:
+    def __init__(self, config: LastFmConfig):
+        self._lastfm = pylast.LastFMNetwork(
+            api_key=config.LASTFM_API_KEY,
+            api_secret=config.LASTFM_API_SECRET
+        )
+
+    def artist_tags(self, artist_name):
+        return self._lastfm.get_artist(artist_name).get_top_tags(limit=10)
+
+
 if __name__ == '__main__':
     print("Hello world")
