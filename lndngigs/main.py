a626efa7590974de2bc982651527cec00bf2dba4
epifab <epifab@gmail.com>
2017-07-17 21:02:37 +0000

    Added Procfile

--- lndngigs/main.py
+++ lndngigs/main.py
@@ -8,15 +8,6 @@ app = Flask(__name__)
 
 @app.route("/slack/gigs", methods=['POST'])
 def slack_gigs():
-    slack_bot.run_command(
-        text="gigs {}".format(request.form["text"]),
-        user=request.form["user_name"],
-        channel=request.form["channel_id"],
-    )
-    return 'OK'
-
-
-if __name__ == '__main__':
     logger = get_logger()
 
     slack_bot = SlackBot(
@@ -28,6 +19,16 @@ if __name__ == '__main__':
         )
     )
 
+    slack_bot.run_command(
+        text="gigs {}".format(request.form["text"]),
+        user=request.form["user_name"],
+        channel=request.form["channel_id"],
+    )
+
+    return 'OK'
+
+
+if __name__ == '__main__':
     app.run(
         host="0.0.0.0",
         port=int(os.getenv('PORT', 5000))
